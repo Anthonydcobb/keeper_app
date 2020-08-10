c6894e6d6f52fb3026fb2ceecf968a4a5988ed49
anthony cobb <anthonycobb@anthonys-MacBook-Pro-2.local>
2020-08-09 21:23:03 -0400

    create map function to display cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,15 @@ import React from "react";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.js";
 
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      {notes.map((notes) => (
+        <Note key={notes.key} title={notes.title} content={notes.content} />
+      ))}
       <Footer />
     </div>
   );
